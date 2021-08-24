33771a60a6852ce2c4cfb58166b424287ea97f78
Matheus Battirola <mcbattirola@hotmail.com>
2021-08-24 20:50:45 -0300

    Initial commit: contracts and create-react-app.

---
+++ contracts/Token.sol
@@ -0,0 +1,65 @@
+pragma solidity ^0.8.0;
+
+import "./SafeMath.sol";
+
+contract Token {
+    using SafeMath for uint256;
+    string public name = "MyToken";
+    string public symbol = "MTO";
+    uint public decimals = 18;
+    uint256 public totalSupply = 1000000000000000000000000; // 1 million tokens
+
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+
+    event Approval(
+        address indexed tokenOwner,
+        address indexed spender,
+        uint256 tokens
+    );
+    event Transfer(address indexed from, address indexed to, uint256 tokens);
+
+    constructor(uint256 total) {
+        totalSupply = total;
+        balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(address _to, uint256 _value)
+        public
+        returns (bool success)
+    {
+        require(_value <= balanceOf[msg.sender]);
+
+        balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);
+        balanceOf[_to] = balanceOf[_to].add(_value);
+
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][_to]);
+
+        balanceOf[_from] = balanceOf[_from].sub(_value);
+        balanceOf[_to] = balanceOf[_to].add(_value);
+        allowance[_from][_to] = allowance[_from][_to].sub(_value);
+
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value)
+        public
+        returns (bool success)
+    {
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+}
